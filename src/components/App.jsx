6191ace61b65521126450afb217b3cb656d1b01f
Igor432 <igomar0602@gmail.com>
2022-09-23 17:55:19 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,6 @@ import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './ImageGallery/Button';
 import Notiflix from 'notiflix';
 
-
 const { Component } = require('react');
 
 class App extends Component {
@@ -17,30 +16,26 @@ class App extends Component {
   };
 
   getPhoto = async (keyWord, page) => {
-
     this.setState({ isLoading: true });
-    const photos = await axios.get(
-      `https://pixabay.com/api/?q=${keyWord}&page=${page}&key=28780636-ee20ed417c8a5aa1eeee48e35&image_type=photo&orientation=horizontal&per_page=12`
-    );
-  
-    this.setState(prev => {
-      return {
-        total: photos.data.total / 12,
-        photos: [...prev.photos, ...photos.data.hits],
-        isLoading: false,
-        page: prev.page + 1,
-      }
-    })
+    try {
+      const photos = await axios.get(
+        `https://pixabay.com/api/?q=${keyWord}&page=${page}&key=28780636-ee20ed417c8a5aa1eeee48e35&image_type=photo&orientation=horizontal&per_page=12`
+      );
 
-if (this.state.photos === [])
-     Notiflix.Notify.failure('Not found');
-
-  
-
+      this.setState(prev => {
+        return {
+          total: photos.data.total / 12,
+          photos: [...prev.photos, ...photos.data.hits],
+          isLoading: false,
+          page: prev.page + 1,
+        };
+      });
+    } catch {
+      console.log('Error');
+    }
   };
 
   onSubmit = e => {
- 
     const keyWord = e.target.search.value;
     this.setState({
       page: 1,
@@ -50,10 +45,9 @@ if (this.state.photos === [])
 
     e.preventDefault();
     e.target.reset();
+
     this.getPhoto(keyWord, 1);
- 
- 
-    
+    console.log(this.state.photos);
   };
 
   onChange = e => {
@@ -67,11 +61,9 @@ if (this.state.photos === [])
   };
 
   render() {
-    const { total } = this.state;
     const { photos } = this.state;
     const { modal } = this.state;
     const { isLoading } = this.state;
-    const { page } = this.state;
 
     return (
       <div className={style.main}>
@@ -88,7 +80,7 @@ if (this.state.photos === [])
           Loading={isLoading}
         />
 
-        {total >= page && <Button loadMore={this.loadMore} />}
+        {photos.length >= 12 && <Button loadMore={this.loadMore} />}
       </div>
     );
   }
